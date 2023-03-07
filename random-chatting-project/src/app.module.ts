188e91d9c1f4d7f4f3252c7f7b7686a008d2a0cb
KANGUK LEE <iamkanguk97@kakao.com>
2023-03-07 20:01:34 +0900

    chore: setting env variable + connect with mongoose + template engine hbs

--- random-chatting-project/src/app.module.ts
+++ random-chatting-project/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
--- random-chatting-project/src/main.ts
+++ random-chatting-project/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));   // useStaticAssets (public)
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));   // 템플릿 엔진을 어느 폴더에 놓을 것인지?
+  app.setViewEngine('hbs');
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
